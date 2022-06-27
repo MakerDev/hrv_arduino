8730eeb0d2af3530c6f4371f373a819f298d02da
YujinShin <raki2001@naver.com>
2022-06-28 00:19:09 +0900

    HRV recorder added.

--- hrv_analyzer/hrv_analyzer.py
+++ hrv_analyzer/hrv_analyzer.py
@@ -1,16 +1,24 @@
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
+from ecgdetectors import Detectors
+from hrv import HRV
 
 if __name__ == "__main__":
     readings = []
     hrvs = []
-
-    with open('hrv_analyzer/raw_readings.csv') as f:
+    #filename = 'hrv_analyzer/raw_readings.csv'
+    filename = 'ppg_logs_final.csv'
+    with open(filename) as f:
         rdr = csv.reader(f)
+        first = True
         for line in rdr:
-            timestamp, reading, hrv = line
-            readings.append(int(reading))
+            if first:
+                first = False
+                continue
+            
+            index, timestamp, ms, reading, hrv = line
+            readings.append(float(reading))
             hrvs.append(0 if 'inf' in hrv else float(hrv))
     
 
@@ -20,4 +28,21 @@ if __name__ == "__main__":
 
     plt.plot(t, hrvs, 'r--', t, readings, 'g')
     plt.ylim([0, 200])
+
+
+    '''
+    ECG DETECTOR TEST
+    '''
+    fs = 150 #CSV 파일 보고 역산
+    detectos = Detectors(fs)
+    r_peaks = detectos.two_average_detector(readings)
+    rr_intervals = np.diff(r_peaks)
+    heart_rates = 60.0/(rr_intervals/100)
+
+    heart_rates_interp = np.interp(t, r_peaks[1:], heart_rates)
+
+    plt.plot(r_peaks, readings[r_peaks], 'bo')
+    plt.plot(t, heart_rates_interp, color='violet')
     plt.show()
+
+    
---
+++ hrv_recorder.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import csv
+import serial
+import time
+import datetime
+import pickle
+
+def get_timestamp():
+    current_time = datetime.datetime.now()
+    current_time_ms = int(time.time()*1000)
+
+    return current_time, current_time_ms
+
+def save_csv(filename, logs):
+    with open(filename, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(logs)
+
+
+if __name__ == '__main__':
+    baud_rate = 9600
+    ser = serial.Serial('COM7', baud_rate)
+    ser.close()
+    ser.open()
+    logs = [['index', "timestamp", "millisec", "raw-value", 'bpm']]
+    
+    fs = 80
+    '''
+    3분에 한 파일 정도 나오도록 설계
+    '''
+    save_period = fs * 60 * 1
+
+    i = 0
+    while True:
+        try:
+            data = str(ser.readline().decode('utf-8')).strip()
+            data = data.split(',')
+
+            if len(data) <= 1:
+                continue
+
+            value, bpm = data
+            dt, ms = get_timestamp()
+            log = [i,dt, ms, value, bpm]
+            logs.append(log)
+            print(log)
+            i += 1
+
+            if i % save_period == 0:
+                save_csv(f'ppg_logs_{i/save_period}.csv', logs[i-save_period:])
+        except KeyboardInterrupt:
+            break
+
+    ser.close()
+
+    with open('raw_dump.pkl', 'wb')  as f:
+        pickle.dump(logs, f)
+
+    save_csv('ppg_logs_final.csv', logs=logs)
+
