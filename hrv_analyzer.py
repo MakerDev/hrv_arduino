ff3dfed30b16448a61b91ec46a4a0a80e6d0c152
YujinShin <raki2001@naver.com>
2022-09-25 03:46:47 +0900

    hrv_analyzer.py: PPG plotting feature upgraded.

--- hrv_analyzer.py
+++ hrv_analyzer.py
@@ -4,6 +4,36 @@ from scipy.signal import find_peaks
 import utils
 import os
 
+def plot_all_ppgs_in_one(individual_folder_path, fs=300):
+    ppgs = os.listdir(individual_folder_path)
+
+    fig = plt.figure()
+
+    for i, ppg in enumerate(ppgs):
+        ppg_path = os.path.join(individual_folder_path, ppg)
+        timestamps, readings = utils.load_readings(ppg_path, offset=0)
+
+        ax = fig.add_subplot(2, 7, i+1)
+        t = np.arange(0., len(readings), 1)
+
+        ax.set_title(ppg.split('_')[1][:-4])
+        ax.plot(t, readings, 'g')
+        # plt.xticks(t, timestamps)
+        # ax.xlim([0, max_pt])
+        ax.set_ylim([60, 160])
+
+        r_peaks, _ = find_peaks(readings, distance=150)
+        rr_intervals = np.diff(r_peaks)
+        heart_rates = 60.0/(rr_intervals/fs)
+
+        heart_rates_interp = np.interp(t, r_peaks[1:], heart_rates)
+
+        ax.plot(r_peaks, readings[r_peaks], 'bo')
+        ax.plot(t, heart_rates_interp, color='violet')
+
+    fig.tight_layout()
+    plt.show()
+
 if __name__ == "__main__":
     # filename = 'ppgs/2022-08-19 17-37-33_박준하_resampled.csv'
     # timestamps, readings = utils.load_readings(filename, offset=0)
@@ -35,32 +65,4 @@ if __name__ == "__main__":
     folders = os.listdir('ppgs_sep')
     for folder in folders:
         individual_folder_path = os.path.join('ppgs_sep', folder)
-        ppgs = os.listdir(individual_folder_path)
-
-        fig = plt.figure()
-
-        for i, ppg in enumerate(ppgs):
-            ppg_path = os.path.join(individual_folder_path, ppg)
-            timestamps, readings = utils.load_readings(ppg_path, offset=0)
-
-            ax = fig.add_subplot(2, 7, i+1)
-            t = np.arange(0., len(readings), 1)
-            max_pt = 20000
-            ax.set_title(ppg.split('_')[1][:-4])
-            ax.plot(t, readings, 'g')
-            #plt.xticks(t, timestamps)
-            # ax.xlim([0, max_pt])
-            ax.set_ylim([60, 160])
-
-            fs = 300 #CSV 파일 보고 역산
-            r_peaks, props = find_peaks(readings, distance=150)
-            rr_intervals = np.diff(r_peaks)
-            heart_rates = 60.0/(rr_intervals/fs)
-
-            heart_rates_interp = np.interp(t, r_peaks[1:], heart_rates)
-
-            ax.plot(r_peaks, readings[r_peaks], 'bo')
-            ax.plot(t, heart_rates_interp, color='violet')
-
-        fig.tight_layout()
-        plt.show()
+        plot_all_ppgs_in_one(individual_folder_path)
