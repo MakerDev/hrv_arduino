a6b4cd036c8d3d5ee6f409d791a9745a5b44a20a
YujinShin <raki2001@naver.com>
2022-10-02 01:39:17 +0900

    Unneccesary training part is removed.

--- models/3d_resnet_gesture_model_eval.py
+++ models/3d_resnet_gesture_model_eval.py
@@ -68,85 +68,6 @@ if __name__ == "__main__":
     DEVICE  = torch.device('cuda:' + str(GPU_NUM) if IS_CUDA else 'cpu')
     # endregion
 
-    writer = SummaryWriter('logs/')
-
-    # # region TRAINING
-    # DM = GestureDataManager(BATCH_SIZE)
-    # TRAIN_GESTURE_DATA, TEST_GESTURE_DATA = DM.Load_Dataset()
-    # TRAIN_LOADER, TEST_LOADER     = DM.Load_DataLoader(TRAIN_GESTURE_DATA, TEST_GESTURE_DATA)
-    # criterion = nn.CrossEntropyLoss().to(DEVICE)
-
-    # MODEL = generate_model(34, n_classes=7)
-
-    # LOAD_MODEL = False
-    # if LOAD_MODEL:
-    #     MODEL.load_state_dict(torch.load("savepoints/savepoint_10_40.2.pth"))
-        
-    # MODEL.to(DEVICE)
-
-    # optimizer = optim.Adam(MODEL.parameters(), lr = LEARNING_RATE, weight_decay = L2WEIGHT_DECAY)
-    
-    # best_acc = 0.0
-    # for epoch in range(1, EPOCHS+1):
-    #     MODEL.train()
-    #     total_loss = []
-    #     total_acc = []
-    #     with tqdm(TRAIN_LOADER, unit='batch') as train_epoch:
-    #         for i, (inputs, targets) in enumerate(train_epoch):
-    #             inputs = inputs.to(DEVICE)
-    #             targets = targets.to(DEVICE)
-    #             outputs = MODEL(inputs)
-
-    #             loss = criterion(outputs, targets)
-    #             acc = utils.calculate_accuracy(outputs, targets)
-    #             total_loss.append(loss.cpu().detach().numpy())
-    #             total_acc.append(acc)
-
-    #             optimizer.zero_grad()
-    #             loss.backward()
-    #             optimizer.step()
-
-    #             train_epoch.set_description(f'Training epoch {epoch}->')            
-    #     writer.add_scalar("Loss/train", np.mean(total_loss), epoch)
-    #     writer.add_scalar("Acc/train", np.mean(total_acc), epoch)
-
-    #     total_loss = []
-    #     total_acc = []
-
-    #     MODEL.eval()
-    #     with torch.no_grad():
-    #         with tqdm(TEST_LOADER, unit='batch') as test_epoch:
-    #             for val_inputs, val_targets in test_epoch:
-    #                 val_inputs = val_inputs.to(DEVICE)
-    #                 val_targets = val_targets.to(DEVICE)
-    #                 val_outputs = MODEL(val_inputs)
-
-    #                 val_loss = criterion(val_outputs, val_targets)
-    #                 val_acc = utils.calculate_accuracy(val_outputs, val_targets)          
-                    
-    #                 total_loss.append(val_loss.cpu().detach().numpy())
-    #                 total_acc.append(val_acc)
-    #                 test_epoch.set_description(f'Evaluating...->')
-
-    #     total_loss_mean = np.mean(total_loss)
-    #     total_acc_mean = np.mean(total_acc)
-    #     writer.add_scalar("Loss/test", total_loss_mean, epoch)
-    #     writer.add_scalar("Acc/test", total_acc_mean, epoch)
-    #     writer.flush()
-
-    #     print(f'loss {total_loss_mean:.3f} | Acc {total_acc_mean:.3f}\n')
-            
-    #     if epoch in [10, 20, 30, 50, 100, 200, 250, 500]:
-    #         torch.save(MODEL.state_dict(), f"savepoints/savepoint_{epoch}_{total_acc_mean*100:.1f}.pth")
-        
-    #     if epoch >= 100 and total_acc_mean > best_acc:
-    #         torch.save(MODEL.state_dict(), f"savepoints/savepoint_{epoch}_{total_acc_mean*100:.1f}.pth")
-    #     best_acc = max(total_acc_mean, best_acc)
-    
-    # writer.close()
-    # torch.save(MODEL.state_dict(), f"savepoints/savepoint_{epoch}.pth")
-    # # endregion
-
     # region EVALUATION
     # evaluation
     DM = GestureDataManager(BATCH_SIZE)
@@ -185,7 +106,7 @@ if __name__ == "__main__":
     total_loss_mean = np.mean(total_loss)
     total_acc_mean = np.mean(total_acc)
     print(f'loss {total_loss_mean:.3f} | Acc {total_acc_mean:.3f}\n')
-    Result_pred = np.array(Result_pred).reshape(-1, 7)
+    Result_pred = np.array(Result_pred).reshape(-1, NUM_CLASSES)
     Result_pred = [np.argmax(pred) for pred in Result_pred]
     Result_pred_np = np.array(Result_pred).reshape(-1, 1).squeeze()
     Result_anno_np = np.array(Result_anno).reshape(-1, 1).squeeze()
